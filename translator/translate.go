97ae3a33ff2f2489ed74e4a4a36b06594feb9035
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-10-19 12:54:46 +0300

    translate first rows

--- translator/translate.go
+++ translator/translate.go
@@ -29,33 +29,66 @@ func (t Translator) Run(ctx context.Context, sourceFilename, targetFilename stri
 			log.Printf("can not close the file %s: %v", sourceFilename, err)
 		}
 	}()
-	translated, err := t.getTranslatedFile(ctx, source)
+	err = t.getTranslatedFile(ctx, source)
 	if err != nil {
 		return err
 	}
-	if err := translated.SaveAs(targetFilename); err != nil {
+	if err := source.SaveAs(targetFilename); err != nil {
 		return fmt.Errorf("can not save a file '%v': %w", targetFilename, err)
 	}
 	return nil
 }
 
-func (t Translator) getTranslatedFile(ctx context.Context, file *excelize.File) (*excelize.File, error) {
-	for _, cell := range []string{"U2"} {
-		finnishText, err := file.GetCellValue("Lauttasaari", cell)
+func (t Translator) getTranslatedFile(ctx context.Context, file *excelize.File) error {
+	for _, sheetName := range file.GetSheetList() {
+		rows, err := file.Rows(sheetName)
 		if err != nil {
-			log.Printf("can not read the cell T2: %v", err)
-			continue
+			return fmt.Errorf("can not get rows for a sheet '%v': %w", sheetName, err)
 		}
-		fmt.Println(finnishText)
-		newCtx, cancel := context.WithTimeout(ctx, time.Second * 10)
-		defer cancel()
-		englishText, err := t.client.GetTranslation(newCtx, "fi", "en", finnishText)
+		defer func() {
+			if err := rows.Close(); err != nil {
+				log.Printf("can not close a sheet '%v'", sheetName)
+			}
+		}()
+		rows.Next()
+		firstRow, err := rows.Columns()
 		if err != nil {
-			log.Printf("can not translate %v: %v", finnishText, err)
-			continue
+			return fmt.Errorf("can not read a first row of a sheet '%v': %w", sheetName, err)
 		}
-		file.SetCellValue("Lauttasaari", cell, englishText)
-		fmt.Println(englishText)
+		translatedValues := []string{}
+		for _, cellValue := range firstRow {
+			if cellValue == "" {
+				translatedValues = append(translatedValues, "")
+				continue
+			}
+			translation, err := t.getTranslation(ctx, cellValue)
+			if err != nil {
+				log.Printf("a translation error: %v", err)
+				continue
+			}
+			log.Printf("receive a translations %v", translation)
+			translatedValues = append(translatedValues, translation)
+		}
+		log.Printf("update a first row: %q\n", translatedValues)
+		if err = file.SetSheetRow(sheetName, "A1", &translatedValues); err != nil {
+			return fmt.Errorf(
+				"can not set a new first row '%v' for a sheet %v: %w", 
+				translatedValues, 
+				sheetName, 
+				err,
+			)
+		}
 	}
-	return file, nil
+	return nil
+}
+
+func (t Translator) getTranslation(ctx context.Context, text string) (string, error) {
+	newCtx, cancel := context.WithTimeout(ctx, time.Second * 10)
+	defer cancel()
+	englishText, err := t.client.GetTranslation(newCtx, "fi", "en", text)
+	if err != nil {
+		err := fmt.Errorf("can not translate %v: %v", text, err)
+		return "", err
+	}
+	return englishText, nil
 }
