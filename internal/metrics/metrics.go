7c614058d66913b21643dd46a6d05bb2bdd90064
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-11-27 18:07:38 +0200

    add a simple counter

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -3,12 +3,17 @@ package metrics
 import "github.com/prometheus/client_golang/prometheus"
 
 type Metrics struct {
-	TelegramUpdateCounter prometheus.Counter
+	ChatUpdates prometheus.Counter
 }
 
 func NewMetrics(registerer prometheus.Registerer) *Metrics {
 	metrics := Metrics{
-		prometheus.NewCounter(prometheus.CounterOpts{}),
+		prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: "helsinki_guide",
+			Name:      "chat_updates",
+			Help:      "number of received chat updates",
+		}),
 	}
+	registerer.MustRegister(metrics.ChatUpdates)
 	return &metrics
 }
