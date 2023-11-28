c62eb8709a3d2ff5ff5effa5fe595bf0d53ebd92
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-11-28 15:04:42 +0200

    add a draft of auth middleware

--- internal/bot/configuration/configuration.go
+++ internal/bot/configuration/configuration.go
@@ -4,7 +4,10 @@ type StartupConfig struct {
 	BotAPIToken     string `env:"BOT_TOKEN,required,notEmpty"`
 	DatabaseURL     string `env:"DATABASE_URL,required,notEmpty"`
 	Debug           bool   `env:"DEBUG"`
-	TGUpdateTimeout int    `env:"UpdateTimeout" envDefault:"60"`
+	TGUpdateTimeout int    `env:"UPDATE_TIMEOUT" envDefault:"60"`
+	MetricsUser     string `env:"METRICS_USER,required,notEmpty"`
+	MetricsPassword string `env:"METRICS_PASSWORD,required,notEmpty"`
+	MetricsPort     int    `env:"METRICS_PORT,required,notEmpty"`
 }
 
 type PopulatorConfig struct {
