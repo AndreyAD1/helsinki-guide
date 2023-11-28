1adc6538018dc45e512ec74d72ae7fc9751271f2
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-11-28 16:47:37 +0200

    add metric envs to a startup script

--- deployments/run.sh
+++ deployments/run.sh
@@ -5,6 +5,9 @@ set -xue
 database_url=$1
 bot_token=$2
 tag=$3
+metrics_user=$4
+metrics_password=$5
+metrics_port=$6
 
 container_name=helsinki-guide
 image_name=andreyad/helsinki-guide:$tag
@@ -21,6 +24,9 @@ docker run \
 --env DEBUG=1 \
 --env DATABASE_URL=$database_url \
 --env BOT_TOKEN=$bot_token \
+--env METRICS_USER=$metrics_user \
+--env METRICS_PASSWORD=$metrics_password \
+--env METRICS_PORT=$metrics_port \
 --network host \
 --log-opt tag=hguide \
 --name $container_name \
