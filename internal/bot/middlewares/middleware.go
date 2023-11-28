f1de1dc7b1efa842b7bd92957cd04a063e1a1626
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-11-28 15:26:43 +0200

    add a basic auth for metrics

--- internal/bot/middlewares/middleware.go
+++ internal/bot/middlewares/middleware.go
@@ -1,9 +1,20 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func GetBasicAuthHandler(next http.Handler, username, password string) http.Handler {
+func GetBasicAuthHandler(
+	next http.Handler,
+	expectedUsername,
+	expectedPassword string,
+) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, password, ok := r.BasicAuth()
+		if !ok || user != expectedUsername || password != expectedPassword {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
