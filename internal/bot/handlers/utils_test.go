e2475ab16d72aed99aa9b5a1bad7a6aa3e54bec5
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-11-30 18:22:27 +0200

    add negative tests

--- internal/bot/handlers/utils_test.go
+++ internal/bot/handlers/utils_test.go
@@ -20,7 +20,7 @@ var extendedBuilding = services.BuildingDTO{
 	HistoryRu: utils.GetPointer("history ru"),
 }
 
-func TestSerializeIntoMessage(t *testing.T) {
+func TestSerializeIntoMessage_positive(t *testing.T) {
 	dummyBuilding := services.BuildingDTO{Address: "osoite"}
 	type args struct {
 		object         any
@@ -30,7 +30,6 @@ func TestSerializeIntoMessage(t *testing.T) {
 		name    string
 		args    args
 		expected    string
-		expectedErr bool
 	}{
 		{
 			"dummy fi",
@@ -40,7 +39,6 @@ Katuosoite: osoite
 Käyttöönottovuosi: no data
 Suunnittelijat: no data
 Rakennushistoria: no data`,
-			false,
 		},
 		{
 			"dummy en",
@@ -50,7 +48,6 @@ Address: osoite
 Completion year: no data
 Authors: no data
 Building history: no data`,
-			false,
 		},
 		{
 			"dummy ru",
@@ -60,7 +57,6 @@ Building history: no data`,
 Год постройки: нет данных
 Авторы: нет данных
 История здания: нет данных`,
-			false,
 		},
 		{
 			"extended fi",
@@ -70,7 +66,6 @@ Katuosoite: test address
 Käyttöönottovuosi: 2023
 Suunnittelijat: Author 1, Author2
 Rakennushistoria: history fi`,
-			false,
 		},
 		{
 			"extended en",
@@ -80,28 +75,67 @@ Address: test address
 Completion year: 2023
 Authors: Author 1, Author2
 Building history: history en`,
-			false,
 		},
 		{
-			"dummy ru",
+			"extended ru",
 			args{extendedBuilding, Russian},
 			`Имя: name ru
 Адрес: test address
 Год постройки: 2023
 Авторы: Author 1, Author2
 История здания: history ru`,
-			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := SerializeIntoMessage(tt.args.object, tt.args.outputLanguage)
-			if tt.expectedErr {
-				require.Error(t, err)
-				return
-			}
 			require.NoError(t, err)
 			require.Equal(t, tt.expected, got)
+		})
+	}
+}
+
+func TestSerializeIntoMessage_negative(t *testing.T) {
+	type testTagStruct struct {
+		field1 string `valueLanguage:"en"`
+	}
+	type testTypeStruct struct {
+		field1 rune `valueLanguage:"all" nameEn:"field1"`
+	}
+	type args struct {
+		object         any
+		outputLanguage outputLanguage
+	}
+	tests := []struct {
+		name    string
+		args    args
+		expectedError    error
+	}{
+		{
+			"not a structure",
+			args{123, English},
+			ErrUnexpectedType,
+		},
+		{
+			"a structure with no field tags",
+			args{args{}, English},
+			ErrNoFieldTag,
+		},
+		{
+			"a structure with no name tag",
+			args{testTagStruct{"test"}, English},
+			ErrNoNameTag,
+		},
+		{
+			"a structure with unexpected field tag",
+			args{testTypeStruct{'A'}, English},
+			ErrUnexpectedFieldType,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := SerializeIntoMessage(tt.args.object, tt.args.outputLanguage)
+			require.ErrorIs(t, err, tt.expectedError)
 		})
 	}
 }
