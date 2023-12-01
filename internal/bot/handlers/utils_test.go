36b9b2b7a023758343bf56ab0d6353d2764a2fc9
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-12-01 13:25:07 +0200

    update the tests

--- internal/bot/handlers/utils_test.go
+++ internal/bot/handlers/utils_test.go
@@ -13,11 +13,26 @@ var extendedBuilding = services.BuildingDTO{
 	NameEn:         utils.GetPointer("name en"),
 	NameRu:         utils.GetPointer("name ru"),
 	Address:        "test address",
+	DescriptionFi: utils.GetPointer("description fi"),
+	DescriptionEn: utils.GetPointer("description en"),
+	DescriptionRu: utils.GetPointer("description ru"),
 	CompletionYear: utils.GetPointer(2023),
 	Authors:        &[]string{"Author 1", "Author2"},
 	HistoryFi:      utils.GetPointer("history fi"),
 	HistoryEn:      utils.GetPointer("history en"),
 	HistoryRu:      utils.GetPointer("history ru"),
+	NotableFeaturesFi: utils.GetPointer("features fi"),
+	NotableFeaturesEn: utils.GetPointer("features en"),
+	NotableFeaturesRu: utils.GetPointer("features ru"),
+	FacadesFi: utils.GetPointer("facades fi"),
+	FacadesEn: utils.GetPointer("facades en"),
+	FacadesRu: utils.GetPointer("facades ru"),
+	DetailsFi: utils.GetPointer("details fi"),
+	DetailsEn: utils.GetPointer("details en"),
+	DetailsRu: utils.GetPointer("details ru"),
+	SurroundingsFi: utils.GetPointer("surroundings fi"),
+	SurroundingsEn: utils.GetPointer("surroundings en"),
+	SurroundingsRu: utils.GetPointer("surroundings ru"),
 }
 
 func TestSerializeIntoMessage_positive(t *testing.T) {
@@ -36,54 +51,84 @@ func TestSerializeIntoMessage_positive(t *testing.T) {
 			args{dummyBuilding, Finnish},
 			`Nimi: no data
 Katuosoite: osoite
+Kerrosluku: no data
 Käyttöönottovuosi: no data
 Suunnittelijat: no data
-Rakennushistoria: no data`,
+Rakennushistoria: no data
+Huomattavia ominaisuuksia: no data
+Julkisivut: no data
+Erityispiirteet: no data
+Ymparistonkuvaus: no data`,
 		},
 		{
 			"dummy en",
 			args{dummyBuilding, English},
 			`Name: no data
 Address: osoite
+Description: no data
 Completion year: no data
 Authors: no data
-Building history: no data`,
+Building history: no data
+Notable features: no data
+Facades: no data
+Interesting details: no data
+Surroundings: no data`,
 		},
 		{
 			"dummy ru",
 			args{dummyBuilding, Russian},
 			`Имя: нет данных
 Адрес: osoite
+Описание: нет данных
 Год постройки: нет данных
 Авторы: нет данных
-История здания: нет данных`,
+История здания: нет данных
+Примечательные особенности: нет данных
+Фасады: нет данных
+Интересные детали: нет данных
+Окрестности: нет данных`,
 		},
 		{
 			"extended fi",
 			args{extendedBuilding, Finnish},
 			`Nimi: name fi
 Katuosoite: test address
+Kerrosluku: description fi
 Käyttöönottovuosi: 2023
 Suunnittelijat: Author 1, Author2
-Rakennushistoria: history fi`,
+Rakennushistoria: history fi
+Huomattavia ominaisuuksia: features fi
+Julkisivut: facades fi
+Erityispiirteet: details fi
+Ymparistonkuvaus: surroundings fi`,
 		},
 		{
 			"extended en",
 			args{extendedBuilding, English},
 			`Name: name en
 Address: test address
+Description: description en
 Completion year: 2023
 Authors: Author 1, Author2
-Building history: history en`,
+Building history: history en
+Notable features: features en
+Facades: facades en
+Interesting details: details en
+Surroundings: surroundings en`,
 		},
 		{
 			"extended ru",
 			args{extendedBuilding, Russian},
 			`Имя: name ru
 Адрес: test address
+Описание: description ru
 Год постройки: 2023
 Авторы: Author 1, Author2
-История здания: history ru`,
+История здания: history ru
+Примечательные особенности: features ru
+Фасады: facades ru
+Интересные детали: details ru
+Окрестности: surroundings ru`,
 		},
 	}
 	for _, tt := range tests {
