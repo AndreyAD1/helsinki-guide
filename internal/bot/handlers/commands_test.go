b3d36a8c94d9d07261a9b5e234734c103f39cbfb
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-12-03 12:58:50 +0200

    add a handler test

--- internal/bot/handlers/commands_test.go
+++ internal/bot/handlers/commands_test.go
@@ -2,16 +2,18 @@ package handlers
 
 import (
 	c "context"
+	"errors"
 	"testing"
 
 	"github.com/AndreyAD1/helsinki-guide/internal/bot/metrics"
 	"github.com/AndreyAD1/helsinki-guide/internal/bot/services"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func TestHandlerContainer_returnAddresses(t *testing.T) {
 	type fields struct {
-		buildingService    services.Buildings
-		bot                internalBot
+		buildingService    *services.Buildings_mock
+		bot                *internalBot_mock
 		HandlersPerCommand map[string]CommandHandler
 		handlersPerButton  map[string]internalButtonHandler
 		commandsForHelp    string
@@ -28,11 +30,36 @@ func TestHandlerContainer_returnAddresses(t *testing.T) {
 		name   string
 		fields fields
 		args   args
+		buildingPreviews []services.BuildingPreview
+		buildingError error
+		expectedMsg tgbotapi.MessageConfig
 	}{
-		// TODO: Add test cases.
+		{
+			"a building error",
+			fields{
+				services.NewBuildings_mock(t),
+				newInternalBot_mock(t),
+				map[string]CommandHandler{},
+				map[string]internalButtonHandler{},
+				"",
+				nil,
+			},
+			args{chatID: 123},
+			[]services.BuildingPreview{},
+			errors.New("some error"),
+			tgbotapi.NewMessage(123, "Internal error"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tt.fields.buildingService.EXPECT().GetBuildingPreviews(
+				tt.args.ctx,
+				tt.args.address,
+				tt.args.limit,
+				tt.args.offset,
+			).Return(tt.buildingPreviews, tt.buildingError)
+			tt.fields.bot.EXPECT().
+			Send(tt.expectedMsg).Return(tgbotapi.Message{}, nil)
 			h := HandlerContainer{
 				buildingService:    tt.fields.buildingService,
 				bot:                tt.fields.bot,
