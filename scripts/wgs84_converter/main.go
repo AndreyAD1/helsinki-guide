48804ee55d020fa03549fa5d4ae266c26c7f90c6
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-12-17 14:12:52 +0200

    add a converte script

---
+++ scripts/wgs84_converter/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	databaseURL string
+	converterURL string
+	RootCmd = &cobra.Command{
+		Use:   "convert",
+		Short: "Convert ETRS89/GK25FIN coordinates into WGS84 coordinate.",
+		Long: `This script gets projected coordinates from columns 'latitude_etrsgk25' and
+'longitude_etrsgk25' of the table 'buildings', converts them into
+geographic coordinates, and sets the columns 'latitude_wgs84' and 
+'longitude_wgs84' respectively.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run()
+		},
+	}
+)
+
+func init() {
+	RootCmd.Flags().StringVarP(
+		&databaseURL,
+		"dburl",
+		"u",
+		"",
+		"A database URL.",
+	)
+	RootCmd.Flags().StringVarP(
+		&converterURL,
+		"converter",
+		"c",
+		"https://epsg.io",
+		"A base URL for a converter service. Default: https://epsg.io",
+	)
+	RootCmd.MarkFlagRequired("dburl")
+}
+
+func main() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run() error {
+	log.Println(databaseURL)
+	log.Println(converterURL)
+	return nil
+}
