7960bda37c16656632d8a412c599c2937164003a
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-12-29 12:57:27 +0200

    additional help improvements

--- internal/bot/handlers/constants.go
+++ internal/bot/handlers/constants.go
@@ -4,13 +4,14 @@ const (
 	headerTemplate = "Search address: %s\nAvailable building addresses and names:"
 	lineTemplate   = "%v. %s - %s"
 	helpMessage    = `Available commands: 
+/start - I will send a greeting message.
 /addresses - I will return all addresses I know.
 /addresses osoi - I will return all addresses that have the prefix "osoi" or "Osoi". You can try any prefix.
 /building osoite 1 - I will return information about all buildings with an address of "Osoite 1". You can try any address.
-/configure - I will return a menu so that you can manage your preferences.
+/settings - I will return a menu so that you can manage your preferences.
 /help - I will show this message.
 
-If you click the button "Share my location and get the nearest buildings", I will return all known addresses that are close to your location."`
+If you click the button "Share my location and get the nearest buildings", I will return all known addresses that are close to your location.`
 )
 
 var handlersPerCommand = map[string]CommandHandler{
