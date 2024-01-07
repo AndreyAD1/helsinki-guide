61648004c1da8b186121ccc0a68f8527c7e911ef
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2024-01-07 13:21:06 +0200

    add another negative test

--- internal/bot/handlers/callbacks_building_test.go
+++ internal/bot/handlers/callbacks_building_test.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/AndreyAD1/helsinki-guide/internal/bot/metrics"
@@ -11,7 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHandlerContainer_button_errWithoutResponse(t *testing.T) {
+func TestHandlerContainer_building_errWithoutResponse(t *testing.T) {
 	tests := []struct {
 		name         string
 		calbackQuery *tgbotapi.CallbackQuery
@@ -53,7 +54,7 @@ func TestHandlerContainer_button_errWithoutResponse(t *testing.T) {
 	}
 }
 
-func TestHandlerContainer_button_unexpectedButtonData(t *testing.T) {
+func TestHandlerContainer_building_unexpectedButtonData(t *testing.T) {
 	calbackQuery := &tgbotapi.CallbackQuery{
 		ID:      "123",
 		From:    &tgbotapi.User{},
@@ -65,7 +66,7 @@ func TestHandlerContainer_button_unexpectedButtonData(t *testing.T) {
 	}{
 		{"no data", ""},
 		{"unexpected data", `{"unknown json": 123}`},
-		{"unexpected building ID", `{"name": "building", "value": "123.3456"}`},
+		{"unexpected building ID", `{"name": "building", "id": "123.3456"}`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -89,4 +90,34 @@ func TestHandlerContainer_button_unexpectedButtonData(t *testing.T) {
 			require.Error(t, err)
 		})
 	}
+}
+
+func TestHandlerContainer_building_serviceError(t *testing.T) {
+	calbackQuery := &tgbotapi.CallbackQuery{
+		ID:      "123",
+		From:    &tgbotapi.User{},
+		Message: &tgbotapi.Message{Chat: &tgbotapi.Chat{ID: 99}},
+		Data:    `{"name": "building", "id": "123"}`,
+	}
+	botMock := NewInternalBot_mock(t)
+	buildingMock := services.NewBuildings_mock(t)
+	botMock.EXPECT().
+		Send(tgbotapi.NewMessage(calbackQuery.Message.Chat.ID, "Internal error")).
+		Return(tgbotapi.Message{}, nil).
+		On("Request", tgbotapi.NewCallback(calbackQuery.ID, "")).
+		Return(nil, nil)
+	ctx := context.Background()
+	buildingMock.EXPECT().GetBuildingByID(ctx, int64(123)).Return(nil, errors.New("test"))
+	h := HandlerContainer{
+		buildingMock,
+		services.NewUsers_mock(t),
+		botMock,
+		map[string]CommandHandler{},
+		map[string]internalButtonHandler{},
+		"",
+		metrics.NewMetrics(prometheus.NewRegistry()),
+		map[string]CommandHandler{},
+	}
+	err := h.building(ctx, calbackQuery)
+	require.Error(t, err)
 }
